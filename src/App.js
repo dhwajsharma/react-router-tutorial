447035aadcc0bac6085923480f9c6f039de33f4c
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-18 17:58:35 +0530

    implemented routing and switch

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
