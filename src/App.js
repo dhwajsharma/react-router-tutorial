f9d0c3995aa6fb6fff05a881255c4daac828e982
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-18 17:39:49 +0530

    adding router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import About from './About';
 import './App.css';
+import Nav from './Nav';
+import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <About />
+      <Shop />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul className="nav-links">
+                <li>About</li>
+                <li>Shop</li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
